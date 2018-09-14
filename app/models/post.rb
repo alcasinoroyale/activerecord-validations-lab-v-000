a902c5dbff8779fe51e45c47c9a52e9dc70741c4
Alex Zdatny <alexzdatny@gmail.com>
2018-09-14 02:52:51 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
@@ -9,6 +10,7 @@ class Post < ActiveRecord::Base
     /Top[0-9]*/i
     /Guess/i
   ]
-  def clickbait?
-    if !ClICKBAIT.any? { |phrase|}
+  
+  def clickbaity?
+    if !ClICKBAIT.none? { |phrase| phrase.match 
 end
