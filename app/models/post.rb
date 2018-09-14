17fb385e8e8c400324191bdb78a12c1e0c471dd4
Alex Zdatny <alexzdatny@gmail.com>
2018-09-14 02:53:48 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ActiveRecord::Base
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
   validate :clickbaity?
+  
   CLICKBAIT = [
     /Won't Believe/i
     /Secret/i
