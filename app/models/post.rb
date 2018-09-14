8f1dd31587a3440860ce94b3d200177a02f1ccb3
Alex Zdatny <alexzdatny@gmail.com>
2018-09-14 02:55:21 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,13 +2,13 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
-  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
   validate :clickbait?
 
   CLICKBAIT = [
-    /Won't Believe/i
-    /Secret/i
-    /Top[0-9]*/i
+    /Won't Believe/i,
+    /Secret/i,
+    /Top[0-9]*/i,
     /Guess/i
   ]
 
