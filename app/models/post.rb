7243a32a8d383ce539e841302cab06ac872ac1af
Alex Zdatny <alexzdatny@gmail.com>
2018-09-14 02:57:04 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,7 +13,7 @@ class Post < ActiveRecord::Base
   ]
 
   def clickbait?
-    if !ClICKBAIT.none? { |phrase| phrase.match title }
+    if !ClICKBAIT.none? { |phrase| phrase.match title}
       errors.add(:title, "must be clickbait")
     end
   end
